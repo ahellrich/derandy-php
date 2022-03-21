5a3a160338ad106a2f3b5d9ff19fa8e33bd524d4
Andreas Hellrich <andreas.hellrich@gmail.com>
2022-03-21 11:30:49 +0100

    worked on wakeup

--- web/wakeUp.php
+++ web/wakeUp.php
@@ -3,7 +3,7 @@
     header("Access-Control-Allow-Headers: *");
     
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        echo "Ich bin wach";
+        echo 42;
         }
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         echo "Schön, dass Sie hier sind, aber hier gibt es nichts zu sehen, die Datei ist für POST Befehle gedacht";
